3b262801f5cc01844b8f4d8a893280efe298fe81
Sergii79 <tsymbal.sergiy@gmail.com>
2023-04-27 20:41:06 +0300

    commit first

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,5 @@
+import { GlobalStyle } from "./GlobalStyle";
+
 export const App = () => {
   return (
     <div
@@ -11,6 +13,7 @@ export const App = () => {
       }}
     >
       React homework template
+      <GlobalStyle />
     </div>
   );
 };
